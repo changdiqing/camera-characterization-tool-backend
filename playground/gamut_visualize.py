836b0014ce96d5446c1b552f2c9bd871486bdad4
Diqing Chang <diqing.chang@tum.de>
2021-12-24 00:34:31 +0100

    initial commit analysis server of the camera characterisation tool

--- playground/gamut_visualize.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import colour
-import matplotlib.pyplot as plt
-RGB = np.random.random((32, 32, 3))
-print(RGB)
-colour.plotting.plot_RGB_chromaticities_in_chromaticity_diagram_CIE1931(
-    [[[]]], colourspaces=['sRGB'])
-plt.show()
